97cdbd2e774fbc14d883b3a564edf6df03dda0d3
Simon DeMartini <simondemartini@gmail.com>
2016-11-10 21:45:02 -0800

    Revert "Add public signatures for Auction class."
    This reverts commit 8f520993df331b915640cd9caa956409d8fc3e73.

--- src/AuctionCentral.java
+++ src/AuctionCentral.java
@@ -1,11 +1,3 @@
-import java.io.Serializable;
-
-/**
- * This class is the main starting point for the AuctionCentral model. It maintains all necessary top level information
- * and methods for other classes to interact with.
- * @author Simon DeMartini
- * @version Nov 7 2016
- */
-public class AuctionCentral implements Serializable{
+public class AuctionCentral {
 
 }
--- src/AuctionItem.java
+++ src/AuctionItem.java
@@ -2,5 +2,5 @@
  * Created by Syndielle on 11/7/2016.
  */
 public class AuctionItem {
-
+    
 }
