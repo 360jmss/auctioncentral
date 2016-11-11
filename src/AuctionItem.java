caf6f684226f48c88fd02666d3f79589ed7f53d3
Samantha Ong <sjfoh@uw.edu>
2016-11-11 12:20:59 -0800

    Initial commit for these classes.

--- src/AuctionItem.java
+++ src/AuctionItem.java
@@ -1,5 +1,6 @@
 /**
- * Created by Syndielle on 11/7/2016.
+ * @author Samantha Ong
+ * @version 11/7/2016.
  */
 public class AuctionItem {
     
---
+++ src/TestAuctionItem.java
@@ -0,0 +1,8 @@
+/**
+ * @author Samantha Ong
+ * @version 11/10/2016.
+ */
+public class TestAuctionItem {
+
+
+}
