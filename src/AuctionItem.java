758f4b61fa1a49262faac267ed276731443e2a73
Samantha Ong <sjfoh@uw.edu>
2016-11-11 12:43:18 -0800

    added myName field.

--- src/AuctionItem.java
+++ src/AuctionItem.java
@@ -1,7 +1,22 @@
 /**
+ * This class represents an AucitonItem and holds necessary information
+ * that an item holds.
  * @author Samantha Ong
  * @version 11/7/2016.
  */
 public class AuctionItem {
+
+    /* Item Name (string), Condition (one of: acceptable, good, very good,
+    like new, new), Size (one of: small (no dimension is greater than one foot),
+    medium (at least one dimension is greater than one foot but no dimension is greater than three feet),
+    large (at least one dimension is greater than three feet) and minimum acceptable bid (positive integer).
+     Optional (non-required) fields are: Donor Name (string), Item Description for Bidders (string),
+      and Comment for Auction Central staff (string).
+     */
+    /**The name of the AucitonItem*/
+    private String myName;
+
+    /**The condition of the item*/
     
+
 }
