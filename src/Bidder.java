79cf2ce66b1b0377af492d8559bb5e3b6998fd64
Jessica Sills <jsills@uw.edu>
2016-11-10 22:46:52 -0800

    Added getter methods and comments to all of the User classes.

--- src/Bidder.java
+++ src/Bidder.java
@@ -3,14 +3,39 @@
  */
 public class Bidder extends User {
 
+    /**
+     * A registered bidder's phone number as a string.
+     */
     private String myPhoneNumber;
-    private String myEmail;
+    /**
+     * A registered bidder's address as a string.
+     */
     private String myAddress;
+    /**
+     * A registered bidder's email address as a string.
+     */
+    private String myEmail;
+    /**
+     * A registered bidder's payment information as a string. Set to "default in the constructor.
+     */
     private String myPaymentInfo;
+    /**
+     * A registered bidder's list of bids.
+     */
     private List<Bid> myBids;
 
-    public Bidder(String theName, String theUsername, String thePhoneNumber,
-                  String theEmail, String theAddress) {
+    /**
+     * Constructs a bidder object. The bidder must have a name, username, phone number, address, and email address
+     * associated with them. In addition, they will have payment information but it will be updated when they bid on an
+     * item.
+     *
+     * @param theName The name of the user.
+     * @param theUsername The username of the user.
+     * @param thePhoneNumber The phone number of the bidder.
+     * @param theAddress The address of the bidder.
+     * @param theEmail The email address of the bidder.
+     */
+    public Bidder(String theName, String theUsername, String thePhoneNumber, String theAddress, String theEmail) {
         myName = theName;
         myUsername = theUsername;
         myPhoneNumber = thePhoneNumber;
@@ -18,5 +43,59 @@ public class Bidder extends User {
         myAddress = theAddress;
         myPaymentInfo = "default";
         myBids = new List<Bid>();
+    }
+
+    /**
+     * Retrieves the name of a user.
+     *
+     * @return The name of a user.
+     */
+    public String getName() {
+        return myName;
+    }
+
+    /**
+     * Retrieves the username of a user.
+     *
+     * @return The username of a user.
+     */
+    public String getUsername() {
+        return myUsername;
+    }
+
+    /**
+     * Retrieves the phone number of a bidder.
+     *
+     * @return The phone number of a bidder.
+     */
+    public String getPhoneNumber() {
+        return myPhoneNumber;
+    }
+
+    /**
+     * Retrieves the address of a bidder.
+     *
+     * @return The address of a bidder.
+     */
+    public String getAddress() {
+        return myAddress;
+    }
+
+    /**
+     * Retrieves the email address of a bidder.
+     *
+     * @return The email address of a bidder.
+     */
+    public String getEmail() {
+        return myEmail;
+    }
+
+    /**
+     * Retrieves the payment information of a bidder.
+     *
+     * @return The payment information of a bidder.
+     */
+    public String getPaymentInfo() {
+        return myPaymentInfo;
     }
 }
--- src/Staff.java
+++ src/Staff.java
@@ -3,8 +3,32 @@
  */
 public class Staff extends User {
 
+    /**
+     * Constructs a Staff object. The staff must have a name and username associated with it.
+     *
+     * @param theName The name of the user.
+     * @param theUsername The username of the user.
+     */
     public Staff(String theName, String theUsername) {
         myName = theName;
         myUsername = theUsername;
+    }
+
+    /**
+     * Retrieves the name of a user.
+     *
+     * @return The name of a user.
+     */
+    public String getName() {
+        return myName;
+    }
+
+    /**
+     * Retrieves the username of a user
+     *
+     * @return The username of a user.
+     */
+    public String getUsername() {
+        return myUsername;
     }
 }
