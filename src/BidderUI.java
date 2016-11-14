8a3fbd50971dd8f4fec30425824f19e02764b9e5
Samantha Ong <sjfoh@uw.edu>
2016-11-13 23:23:59 -0800

    Created BidderUI.
    Added displayHeader, displayAuctionList, displayMenu

--- src/BidderUI.java
+++ src/BidderUI.java
@@ -1,7 +1,77 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 /**
- * Created by Syndielle on 11/13/2016.
+ * This is the Bidder UI.
+ * @author Samantha Ong
+ * @version 11/13/2016
  */
-public class BidderUI {
+public final class BidderUI {
+
+    private User myUser;
+
+    private Calendar myCalendar;
+
+    /**
+     * The Bidder UI.
+     * @param theUser the user
+     * @param theCalendar the calendar
+     */
+    public BidderUI(User theUser, Calendar theCalendar) {
+        myUser = theUser;
+        myCalendar = theCalendar;
+        displayMenu();
+    }
+
+    /**
+     * Displays the main menu.
+     */
+    private void displayMenu() {
+        displayHeader();
+        System.out.println("What would you like to do?");
+        System.out.println("1. View upcoming auctions");
+        System.out.println("2. Log out");
+    }
+
+    /**
+     * Displays a list of upcoming auctions.
+     */
+    private void displayAuctionList() {
+        displayHeader();
+        System.out.println("Below are the list of auctions:");
+        List<Auction> auctionList = myCalendar.getAuctions(LocalDateTime.now());
+        for(int i = 1; i < myCalendar.getAuctions().size(); i++) {
+            System.out.println(i + ". " + auctionList.get(i - 1).toString());
+        }
+        System.out.println("\nWhat would you like to do?");
+        System.out.println("1. Bid on an auction");
+        System.out.println("2. View my bids");
+        System.out.println("3. Go back");
+    }
+
+    /**
+     * Displays the view for bidding on an auction.
+     */
+    private void displayBidOnAuction() {
+        //TODO
+    }
+
+    /**
+     * Displays the users bids.
+     */
+    private void displayViewMyBids() {
+        //TODO
+    }
+
+    /**
+     * Displays the header of the UI.
+     */
+    private void displayHeader() {
+        System.out.println("AuctionCentral: the auctioneer for non-profit organizations.");
+        System.out.println(myUser.getName() + " logged in as Bidder\n");
+        System.out.println(LocalDate.now().toString() + "\n");
+    }
+
 }
