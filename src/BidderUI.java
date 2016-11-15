3f0b9b22f9d96d65b4a6bf1da0eb80b5e9c08165
Samantha Ong <sjfoh@uw.edu>
2016-11-14 21:06:07 -0800

    fixed bidderUI go back view for auctions.

--- src/BidderUI.java
+++ src/BidderUI.java
@@ -32,11 +32,10 @@ public final class BidderUI {
         myAuctionList = myCalendar.getAuctions(LocalDateTime.now());
     }
 
-
     /**
-     * Displays a list of upcoming auctions.
+     * Prints the view for Auction List view.
      */
-    private void displayAuctionList() {
+    private void printAuctionListView() {
         displayHeader();
         System.out.println("Below are the list of auctions:");
         for(int i = 1; i < myCalendar.getAuctions().size(); i++) {
@@ -44,22 +43,29 @@ public final class BidderUI {
         }
         System.out.println("\nWhat would you like to do?");
         System.out.println("1. View an auction");
-        System.out.println("2. View my bids");
-        System.out.println("3. Go back");
+        System.out.println("2. Go back");
+    }
+
+    /**
+     * Displays a list of upcoming auctions.
+     */
+    private void displayAuctionList() {
+        printAuctionListView();
         int menuChoice;
         do {
             menuChoice = getMenuChoice(2);
             if (menuChoice == 1) {
                 displayChooseAuctionView();
+                printAuctionListView();
             }
         } while (menuChoice != 2);
-
     }
 
     /**
      * Displays the View to choose an auction.
      */
     private void displayChooseAuctionView() {
+        displayHeader();
         System.out.println("Which auction would you like to View?");
         System.out.println("Enter 0 to go back.");
         int menuChoice;
@@ -80,7 +86,7 @@ public final class BidderUI {
         System.out.println("Items offered for sale:");
         System.out.format("%8s%25s%12s%10s%10s","ID", "Item Name", "Condition", "Min Bid", "My Bid");
         for (AuctionItem item : theAuction.getItems()) {
-            System.out.format("%8d%25s%12s%8.2f%8.2f", item.getUniqueID(), item.getName(),
+            System.out.format("%8d%25s%12s%8.2f%8.2f\n", item.getUniqueID(), item.getName(),
                     item.getCondition(), item.getMinBid(), item.getBid(myUser.getName()));
         }
         System.out.println("\n\nWhat would you like to do?");
@@ -101,15 +107,14 @@ public final class BidderUI {
      * @param theItemList the list of items from the auction.
      */
     private void displayChooseItemView(List<AuctionItem> theItemList) {
+        displayHeader();
         System.out.println("Which item would you like to view?");
-        System.out.println("Enter 0 to go back.");
         int menuChoice;
-        do {
-            menuChoice = getMenuChoice(theItemList.size());
-            if (menuChoice <= theItemList.size() && menuChoice > 0) {
-                displayViewAnItemView(theItemList.get(menuChoice-1));
-            }
-        } while (menuChoice != 0);
+        menuChoice = getMenuChoice(theItemList.size());
+        if (menuChoice <= theItemList.size() && menuChoice > 0) {
+            displayViewAnItemView(theItemList.get(menuChoice-1));
+        }
+
     }
 
     /**
@@ -117,6 +122,7 @@ public final class BidderUI {
      * @param theItem the item we are viewing.
      */
     private void displayViewAnItemView(AuctionItem theItem) {
+        displayHeader();
         System.out.println(theItem.toString());
         if (theItem.getComment() != null) {
             System.out.println(theItem.getComment());
@@ -138,6 +144,7 @@ public final class BidderUI {
      * Displays the bid on item view.
      */
     private void displayBidOnItemView(AuctionItem theItem) {
+        displayHeader();
         System.out.println(theItem.toString());
         System.out.println("This items minimum bid: " + theItem.getMinBid());
         System.out.println("\nPlease enter your bid: (Please enter bids with decimal value, ie. 5.00");
@@ -179,20 +186,28 @@ public final class BidderUI {
      */
     private void displayHeader() {
         System.out.println("AuctionCentral: the auctioneer for non-profit organizations.");
-        System.out.println(myUser.getName() + " logged in as Bidder\n");
-        System.out.println(LocalDate.now().toString() + "\n");
+        System.out.println(myUser.getName() + " logged in as Bidder");
+        System.out.println("Todays Date: " + LocalDate.now().toString() + "\n");
     }
 
-    public void start() {
-        int menuChoice;
+    /**
+     * Displays the start menu.
+     */
+    private void displayStartMenu() {
         displayHeader();
         System.out.println("What would you like to do?");
         System.out.println("1. View upcoming auctions");
         System.out.println("2. Log out");
+    }
+
+    public void start() {
+        int menuChoice;
+        displayStartMenu();
         do {
             menuChoice = getMenuChoice(2);
             if (menuChoice == 1) {
                 displayAuctionList();
+                displayStartMenu();
             }
         } while (menuChoice != 2);
         S.close();
