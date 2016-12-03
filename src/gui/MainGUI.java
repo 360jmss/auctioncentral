091aa9d65ea29e3177efe37f963d980a9895c075
Simon DeMartini <simondemartini@gmail.com>
2016-12-02 17:23:07 -0800

    Add empty user panels to help get things started

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,12 +1,10 @@
 package gui;
 
-import model.Calendar;
-import model.User;
-import model.UserRepo;
+import model.*;
 
+import javax.smartcardio.Card;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -22,6 +20,18 @@ public class MainGUI implements Observer {
     /** The minimum width of the GUI */
     public static final int DEFAULT_WINDOW_HEIGHT = 500;
 
+    /** The String to access the bidder view */
+    private static final String BIDDER_PANEL = "bp";
+
+    /** The String to access the bidder view */
+    private static final String CONTACT_PANEL = "cp";
+
+    /** The String to access the bidder view */
+    private static final String STAFF_PANEL = "sp";
+
+    /** The String to access the bidder view */
+    private static final String LOGIN_PANEL = "lp";
+
     /** The master list of all users that can log in. */
     private UserRepo myRepo;
 
@@ -32,10 +42,16 @@ public class MainGUI implements Observer {
     private JFrame myFrame;
 
     /** The main chooser for the views */
-    private JPanel myViews, myLoginPanel;
+    private JPanel myMainPanel, myLoginPanel;
 
     /** The status panel */
     private StatusPanel myStatusPanel;
+
+    /** The bidder panel */
+    private UserPanel myBidderPanel, myStaffPanel, myContactPanel;
+
+    /** Te card Layout */
+    private CardLayout myCards;
 
 
     /**
@@ -47,9 +63,13 @@ public class MainGUI implements Observer {
         myRepo = theRepo;
         myCalendar = theCalendar;
         myFrame = new JFrame();
-        myViews = new JPanel(new CardLayout());
+        myCards = new CardLayout();
+        myMainPanel = new JPanel(myCards);
         myStatusPanel = new StatusPanel();
         myLoginPanel = new LoginPanel(myRepo);
+        myBidderPanel = new BidderPanel(myCalendar);
+        myStaffPanel = new StaffPanel(myCalendar);
+        myContactPanel = new ContactPanel(myCalendar);
     }
 
     /**
@@ -62,11 +82,15 @@ public class MainGUI implements Observer {
         myFrame.setMinimumSize(new Dimension(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT));
 
         //setup card layout for user views
-        myViews.add(myLoginPanel);
+        myMainPanel.add(myLoginPanel, LOGIN_PANEL);
+        myMainPanel.add(myBidderPanel, BIDDER_PANEL);
+        myMainPanel.add(myStaffPanel, STAFF_PANEL);
+        myMainPanel.add(myContactPanel, CONTACT_PANEL);
+
 
         //add elements
         myFrame.add(myStatusPanel, BorderLayout.NORTH);
-        myFrame.add(myViews, BorderLayout.CENTER);
+        myFrame.add(myMainPanel, BorderLayout.CENTER);
 
         //add observers
         myRepo.addObserver(this);
@@ -82,6 +106,16 @@ public class MainGUI implements Observer {
         //login
         if(arg instanceof User){
             myStatusPanel.updateUser((User) arg);
+            if(arg instanceof Staff) {
+                myCards.show(myMainPanel, STAFF_PANEL);
+                myStaffPanel.setUser((Staff) arg);
+            } else if (arg instanceof Contact) {
+                myCards.show(myMainPanel, CONTACT_PANEL);
+                myContactPanel.setUser((Contact) arg);
+            } else if (arg instanceof Bidder) {
+                myCards.show(myMainPanel, BIDDER_PANEL);
+                myBidderPanel.setUser((Bidder) arg);
+            }
         }
     }
 }
