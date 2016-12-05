e1428fe6c455aa07b302e69eeb4be145e961699a
name <email>
2016-12-04 21:07:46 -0800

    - Added 3 acceptance tests for User Story #4 from both Check-in 2 and 3, as well as updated the test suite to include them

---
+++ src/test/ChangeMaxNumberOfAuctionsAcceptanceTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.*;
+
+/**
+ * This is a JUnit test class for changing the max number of auctions in the system.
+ *  It cannot be tested because it is handled by the UI
+ * @author Miguel Barreto
+ * @version December 4 2016
+ */
+public class ChangeMaxNumberOfAuctionsAcceptanceTest {
+
+    @Test
+    public void testChangeMaxNumberOfAuctions() {
+        //Cannot be tested.
+    }
+
+}
