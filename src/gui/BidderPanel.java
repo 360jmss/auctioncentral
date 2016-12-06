51ec994acef6300958ac4aa14f3d3583d68c25cd
Samantha Ong <sjfoh@uw.edu>
2016-12-05 17:23:48 -0800

    Fixed Bugs, added cancel bid

--- src/gui/BidderPanel.java
+++ src/gui/BidderPanel.java
@@ -11,6 +11,8 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class BidderPanel extends UserPanel  {
 
@@ -59,7 +61,7 @@ public class BidderPanel extends UserPanel  {
         myCalendar = theCalendar;
         myUser = theUser;
         myActionButtons = new ActionButtons();
-        myLabel = new JLabel("Hi " + myUser.getName() + ", what would you like to do?");
+        myLabel = new JLabel("Hi " + myUser.getName() + ", ");
         mySouthOfBidderPanel = new JPanel();
         myCenterOfBidderPanel = new JPanel();
         myCenterOfBidderPanel.setLayout(new BorderLayout());
@@ -82,7 +84,8 @@ public class BidderPanel extends UserPanel  {
      * @return The created panel
      */
     private JPanel makeAuctionListPanel() {
-        Auction[] auctions = myCalendar.getAuctions().toArray(new Auction[0]);
+        Auction[] auctions =  new Auction[myCalendar.getAuctions(LocalDateTime.now()).size()];
+        auctions = myCalendar.getAuctions(LocalDateTime.now()).toArray(auctions);
         myAuctionsList = new JList<>(auctions);
 
         myListSelectionModel = myAuctionsList.getSelectionModel();
@@ -99,20 +102,51 @@ public class BidderPanel extends UserPanel  {
     }
 
     /**
+     * Creates a panel to hold the specified list of items in an auciton.
+     *
+     * @return The created panel
+     */
+    private JPanel makeAuctionItemListPanel() {
+        AuctionItem[] items =
+                new AuctionItem[myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex).getItems().size()];
+        items = myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex).getItems().toArray(items);
+        myItemList = new JList<>(items);
+        //myItemList.setFont(new Font("monospaced", Font.PLAIN, 12));
+        myListSelectionModel = myItemList.getSelectionModel();
+        myListSelectionModel.addListSelectionListener(
+                new AuctionItemListSelectionHandler()
+        );
+        myListSelectionModel.setSelectionMode(myListSelectionModel.SINGLE_SELECTION);
+        final JScrollPane sp = new JScrollPane(myItemList);
+        JLabel label = new JLabel();
+        sp.setColumnHeaderView(new JLabel(String.format("%5s%45s%30s%50s", "ID", "Name", "Condition", "Minimum Bid")));
+        final JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.add(sp, BorderLayout.CENTER);
+
+        return p;
+    }
+
+
+    /**
      * Creates the about item panel.
      *
      * @return the about item panel made.
      */
     private JPanel makeAboutItemPanel() {
         StringBuilder sb = new StringBuilder();
-        AuctionItem i = myCalendar.getAuctions().get(myAuctionIndex).getItems().get(myAuctionItemIndex);
+        AuctionItem i = myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex).getItems().get(myAuctionItemIndex);
         sb.append("Item Unique ID: " + i.getUniqueID() + "\n");
         sb.append("Item Name: "
                 + i.getName() + "\n");
         sb.append("Item Condition: "
                 + i.getCondition() + "\n");
-        sb.append("Item Size: " + i.getSize());
+        sb.append("Item Size: " + i.getSize() + "\n");
         sb.append(String.format("Item Minimum Bid: %.2f\n", i.getMinBid()));
+        if(i.getBid(myUser.getName()) != null) {
+            sb.append("My bid: " + i.getBid(myUser.getName()));
+        }
+
 
 
         JPanel p = new JPanel();
@@ -151,36 +185,15 @@ public class BidderPanel extends UserPanel  {
         }
     }
 
-    /**
-     * Creates a panel to hold the specified list of items in an auciton.
-     *
-     * @return The created panel
-     */
-    private JPanel makeAuctionItemListPanel() {
-        AuctionItem[] items = new AuctionItem[myCalendar.getAuctions().get(myAuctionIndex).getItems().size()];
-        items = myCalendar.getAuctions().get(myAuctionIndex).getItems().toArray(items);
-        myItemList = new JList<>(items);
 
-        myListSelectionModel = myItemList.getSelectionModel();
-        myListSelectionModel.addListSelectionListener(
-                new AuctionItemListSelectionHandler()
-        );
-        myListSelectionModel.setSelectionMode(myListSelectionModel.SINGLE_SELECTION);
-        final JScrollPane sp = new JScrollPane(myItemList);
-        final JPanel p = new JPanel();
-        p.setLayout(new BorderLayout());
-        p.add(sp, BorderLayout.CENTER);
-
-        return p;
-    }
 
 
     /**
      * Update the auction Item list panel
      */
     private void updateAuctionItemListPanel() {
-        AuctionItem[] items = new AuctionItem[myCalendar.getAuctions().get(myAuctionIndex).getItems().size()];
-        items = myCalendar.getAuctions().get(myAuctionIndex).getItems().toArray(items);
+        AuctionItem[] items = new AuctionItem[myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex).getItems().size()];
+        items = myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex).getItems().toArray(items);
         myItemList.setListData(items);
     }
 
@@ -210,6 +223,9 @@ public class BidderPanel extends UserPanel  {
         /**place bid button*/
         private JButton myPlaceBidButton;
 
+        /**cancel bid button*/
+        private JButton myCancelBidButton;
+
         /**
          * Action Button constructor.
          */
@@ -217,10 +233,12 @@ public class BidderPanel extends UserPanel  {
             super();
             myViewAuctionsButton = new JButton("View Auctions");
             myViewItemsButton = new JButton("View Auction Items");
-            myBidOnItemButton = new JButton("Bid on Selected Item");
+            myBidOnItemButton = new JButton("View/Cancel/Bid on Item");
+            myCancelBidButton = new JButton("Cancel Bid");
             myGoBackButton = new JButton("Go Back");
             myPlaceBidButton = new JButton("Place Bid");
             myNewBidLabel = new JLabel("New Bid: ");
+
             myBidTextField = new JTextField(20);
             setUp();
         }
@@ -235,6 +253,8 @@ public class BidderPanel extends UserPanel  {
             myBidOnItemButton.addActionListener(new BidOnItemListener());
             myBidTextField.addActionListener(new PlaceBidActionListener());
             myPlaceBidButton.addActionListener(new PlaceBidActionListener());
+            myCancelBidButton.addActionListener(new CancelBidActionListener());
+
 
             add(myNewBidLabel);
             add(myBidTextField);
@@ -242,6 +262,7 @@ public class BidderPanel extends UserPanel  {
             add(myViewItemsButton);
             add(myBidOnItemButton);
             add(myPlaceBidButton);
+            add(myCancelBidButton);
             add(myGoBackButton);
 
             myViewItemsButton.setVisible(false);
@@ -250,6 +271,7 @@ public class BidderPanel extends UserPanel  {
             myNewBidLabel.setVisible(false);
             myBidTextField.setVisible(false);
             myPlaceBidButton.setVisible(false);
+            myCancelBidButton.setVisible(false);
         }
 
         /**
@@ -259,16 +281,41 @@ public class BidderPanel extends UserPanel  {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 Double text = Double.parseDouble(myBidTextField.getText());
-                Auction a = myCalendar.getAuctions().get(myAuctionIndex);
+                Auction a = myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex);
                 AuctionItem i = a.getItems().get(myAuctionItemIndex);
                 boolean addedSuccessfully = i.addBid(myUser.getName(), text);
                 if(addedSuccessfully) {
                     myBid = text;
                     System.out.println("Current bid updated: $" + myBid);
                     System.out.println("Added bid: " + i.getBid(myUser.getName()));
+                    myCenterOfBidderPanel.remove(myBidOnItemPanel);
+                    myBidOnItemPanel = makeAboutItemPanel();
+                    myCenterOfBidderPanel.add(myBidOnItemPanel);
+                    myBidOnItemPanel.revalidate();
+                    myBidOnItemPanel.repaint();
                 } else {
                     JOptionPane.showMessageDialog(null,
                             String.format("Error: Please enter bid higher than miniminum bid: $%.2f", i.getMinBid()),
+                            "Error Massage", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+
+        class CancelBidActionListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Auction a = myCalendar.getAuctions(LocalDateTime.now()).get(myAuctionIndex);
+                AuctionItem i = a.getItems().get(myAuctionItemIndex);
+                int cancelledSuccessfully = i.cancelBid(myUser.getName());
+                if(cancelledSuccessfully == 0) {
+                    myCenterOfBidderPanel.remove(myBidOnItemPanel);
+                    myBidOnItemPanel = makeAboutItemPanel();
+                    myCenterOfBidderPanel.add(myBidOnItemPanel);
+                    myBidOnItemPanel.revalidate();
+                    myBidOnItemPanel.repaint();
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Error: You had no bid to begin with.",
                             "Error Massage", JOptionPane.ERROR_MESSAGE);
                 }
             }
@@ -284,6 +331,8 @@ public class BidderPanel extends UserPanel  {
                 myViewItemsButton.setVisible(true);
                 myGoBackButton.setVisible(true);
                 myCenterOfBidderPanel.add(myAuctionListPanel);
+                myLabel.setText("Please select an auction from the list that you would like to view.");
+
             }
         }
 
@@ -296,8 +345,10 @@ public class BidderPanel extends UserPanel  {
                 myViewItemsButton.setVisible(false);
                 myBidOnItemButton.setVisible(true);
                 myAuctionListPanel.setVisible(false);
+                myAuctionItemListPanel.setVisible(true);
                 updateAuctionItemListPanel();
                 myCenterOfBidderPanel.add(myAuctionItemListPanel);
+                myLabel.setText("Please select the item from the list that you would like to view or bid on.");
             }
         }
 
@@ -312,19 +363,24 @@ public class BidderPanel extends UserPanel  {
                     myViewAuctionsButton.setVisible(true);
                     myGoBackButton.setVisible(false);
                     myAuctionListPanel.setVisible(false);
+                    myLabel.setText("Hi " + myUser.getName() + ",");
                 } else if(myBidOnItemButton.isVisible()) { //item list panel
                     myBidOnItemButton.setVisible(false);
                     myViewItemsButton.setVisible(true);
                     myAuctionListPanel.setVisible(true);
                     myAuctionItemListPanel.setVisible(false);
+                    myLabel.setText("Please select an auction from the list that you would like to view.");
                 } else if(myPlaceBidButton.isVisible()) { //about item panel
                     myBidOnItemButton.setVisible(true);
                     myViewItemsButton.setVisible(false);
                     myPlaceBidButton.setVisible(false);
+                    myCancelBidButton.setVisible(false);
                     myBidTextField.setVisible(false);
                     myAuctionItemListPanel.setVisible(true);
                     myBidOnItemPanel.setVisible(false);
                     myNewBidLabel.setVisible(false);
+                    myLabel.setText("Please select the item from the list that you would like to view or bid on.");
+
                 }
             }
         }
@@ -341,7 +397,9 @@ public class BidderPanel extends UserPanel  {
                 myNewBidLabel.setVisible(true);
                 myBidTextField.setVisible(true);
                 myPlaceBidButton.setVisible(true);
+                myCancelBidButton.setVisible(true);
                 myBidOnItemButton.setVisible(false);
+                myLabel.setText("If you bid, please bid at least the minimum bid.");
             }
         }
     }
